5fa215034352e753354ebc00aab56866d374a6ad
AkshataPuranik123 <60555146+AkshataPuranik123@users.noreply.github.com>
2020-08-17 18:24:15 -0400

    Add files via upload

--- test/auction.js
+++ test/auction.js
@@ -24,9 +24,27 @@
             assert.equal(item[2], "18th Century lost masterpiece with blue-and-white floral design", "contains the correct item description"); 
 			assert.equal(item[3], 100, "contains the correct base price");
 			assert.equal(item[4], 10, "contains the correct increment value");
-			assert.equal(item[5], 0, "contains the correct final price");
+			assert.equal(item[5], 100, "contains the correct final price");
         });
     });
 	
-	
+	it("allows a bidder to bid", function() {
+    return Auction.deployed().then(function(instance) {
+      auctionInstance = instance;
+      itemId = 1;
+	  bidAmt = 120;
+      return auctionInstance.bid(itemId, bidAmt);
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "an event was triggered");
+      assert.equal(receipt.logs[0].event, "bidEvent", "the event type is correct");
+      assert.equal(receipt.logs[0].args._itemId.toNumber(), itemId, "the item id is correct");
+	  assert.equal(receipt.logs[0].args._bidAmt.toNumber(), bidAmt, "the amount is correct");
+	  
+	}).then(function(item) {
+        assert(bidAmt, "New highest Bid");
+    })
+  });
+// throwing exception error for 
+// a) invalid entry of bid value
+// b) other exceptions
 });
