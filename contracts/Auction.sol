64cef330b3cbfccd00be4f119d393bff3c41843d
AkshataPuranik123 <60555146+AkshataPuranik123@users.noreply.github.com>
2020-08-17 18:24:57 -0400

    Add files via upload

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -15,21 +15,57 @@ contract Auction {
 // Fetch Items
 	mapping(uint => Item) public items;
 	
-// Store Item Count
-	uint public itemCount;
+// declare variables
+	uint public itemCount;	
+	uint diff;
+	uint a;
+	
+// Bid event
+	event bidEvent(uint _itemId, uint indexed _bidAmt);
+	
 
 // Constructor
 	constructor() public {	
-		addItem("Vase", "18th Century lost masterpiece with blue-and-white floral design", 100, 10);
+		addItem("Vase", "18th Century lost masterpiece with blue-and-white floral design", 100, 10, 100);
 		//addItem("Painting");
 	}
   
 // Function for more than 1 itemCount
-	function addItem (string memory _name, string memory _desc, uint _baseValue, uint _increment) private {
+	function addItem (string memory _name, string memory _desc, uint _baseValue, uint _increment, uint _startPrice) private {
 		itemCount ++;
-		
-		items[itemCount] = Item(itemCount, _name, _desc, _baseValue, _increment, 0);
-	
+		items[itemCount] = Item(itemCount, _name, _desc, _baseValue, _increment,_startPrice);
 	}
+	
+// Function to place a bid
+	function bid (uint _itemId, uint _bidAmt) public {
+	
+		require(_itemId > 0 && _itemId <= itemCount, "Bidding on an invalid item");
+		
+		require(check_bid (_itemId, _bidAmt),"Bid is lower or equal to the highest bid value");
+		
+		require(check_increment (_itemId, _bidAmt),"Minimum increment value is $10");
+		
+		items[_itemId].auction_price = _bidAmt;	
+		
+		//trigger bid event
+		emit bidEvent(_itemId, _bidAmt);
+		
+	}
+	
+// Function to check if the bid is greater than highest bid
+	function check_bid (uint _itemId, uint _bidAmt) public returns (bool) {
+		a = items[_itemId].auction_price;
+		if (_bidAmt > a) return true;
+		else return false;
+	}
+	
+// Function to check if the difference is greater to minimum increment value
+	function check_increment (uint _itemId, uint _bidAmt) public returns (bool) {
+		a = items[_itemId].auction_price;
+		diff = _bidAmt - a;
+		if (diff >= items[_itemId].min_increment) return true;
+		else return false;
+	}
+	
   
 }
